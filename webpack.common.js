6b8bdf81910f3293021c78e35c4be7f05f5decf7
Fernando Santos <fern@ndo.io>
2020-02-06 14:28:00 -0500

    Fix para o Bug no File-Loader

--- webpack.common.js
+++ webpack.common.js
@@ -24,7 +24,8 @@ module.exports = {
           loader: "file-loader",
           options: {
             name: "[name].[hash].[ext]",
-            outputPath: "imgs"
+            outputPath: "imgs",
+            esModule: false
           }
         }
       },
